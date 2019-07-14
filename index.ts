cedab40a7b0217a2ba3a1c5159450b0425b91c88
Umut Özel <umutozel@outlook.com>
2019-07-14 13:24:17 +0300

    fix index

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
-import { Macan, Porsche, sayModel } from "./lib/types";
+import { Cayman, Macan, Porsche, sayModel } from "./lib/types";
 
 sayModel(new Porsche("Targa 4S", 2016));
 
 sayModel(new Macan(2016));
+
+sayModel(new Cayman(2018, { launchControl: true, sport: false, sportPlus: true }));
