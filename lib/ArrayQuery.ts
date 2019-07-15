a209ab77da54f2d534ee30f7e86ba15529bb56a3
Umut Özel <umutozel@outlook.com>
2019-07-15 17:08:18 +0300

    refactor

--- lib/ArrayQuery.ts
+++ lib/ArrayQuery.ts
@@ -13,35 +13,52 @@ export class ArrayQuery<T> implements IQuery<T> {
 
     private expressions: IExpression[] = [];
 
-    constructor(private array: T[], expressions: IExpression[] = []) {
+    constructor(private array: T[], expressions: IExpression[] = []) {
         this.expressions = expressions;
     }
 
     public where(predicate: (x: T) => boolean): IQuery<T> {
-        const newExpressions = [...this.expressions, { type: ExpressionType.where, value: predicate }];
-
-        return new ArrayQuery<T>(this.array, newExpressions);
+        return this.clone(ExpressionType.where, predicate);
     }
 
     public select<TOut>(selector: (x: T) => TOut): IQuery<TOut> {
-        const newExpressions = [...this.expressions, { type: ExpressionType.select, value: selector }];
-
-        return new ArrayQuery<TOut>(this.array as any, newExpressions);
+        return this.clone(ExpressionType.select, selector);
     }
 
     public skip(value: number): IQuery<T> {
-        const newExpressions = [...this.expressions, { type: ExpressionType.skip, value }];
-
-        return new ArrayQuery<T>(this.array, newExpressions);
+        return this.clone(ExpressionType.skip, value);
     }
 
     public take(value: number): IQuery<T> {
-        const newExpressions = [...this.expressions, { type: ExpressionType.take, value }];
-
-        return new ArrayQuery<T>(this.array, newExpressions);
+        return this.clone(ExpressionType.take, value);
     }
 
     public toArray(): T[] {
-        throw new Error("Method not implemented.");
+        let retVal: any[] = this.array;
+
+        for (const exp of this.expressions) {
+            switch (exp.type) {
+                case ExpressionType.where:
+                    retVal = retVal.filter(exp.value);
+                    break;
+                case ExpressionType.select:
+                    retVal = retVal.map(exp.value);
+                    break;
+                case ExpressionType.skip:
+                    retVal = retVal.slice(exp.value);
+                    break;
+                case ExpressionType.take:
+                    retVal = retVal.slice(0, exp.value);
+                    break;
+            }
+        }
+
+        return retVal;
+    }
+
+    private clone<TOut = T>(type: ExpressionType, value: any) {
+        const newExpressions = [...this.expressions, { type: ExpressionType.skip, value }];
+
+        return new ArrayQuery<TOut>(this.array as any, newExpressions);
     }
 }
